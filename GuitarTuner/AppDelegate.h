e3f42646e341a81f764f2f6904b839ed247816a0
Sundy <Sundy@dhcp-10-5-28-53.wireless.rochester.edu>
2015-04-09 13:17:51 -0400

    Initial Commit

---
+++ GuitarTuner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GuitarTuner
+//
+//  Created by Sundy on 4/9/15.
+//  Copyright (c) 2015 Yuhui Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GuitarTuner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GuitarTuner
+//
+//  Created by Sundy on 4/9/15.
+//  Copyright (c) 2015 Yuhui Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
